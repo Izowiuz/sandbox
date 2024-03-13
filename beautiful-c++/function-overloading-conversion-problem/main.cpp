5c2807a53d91dd5a87179362604dd2ccd606dc34
Jakub Narolewski <izowiuz@gmail.com>
2024-03-13 22:41:53 +0100

    new stuff!

---
+++ beautiful-c++/function-overloading-conversion-problem/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <string>
+
+#include <spdlog/spdlog.h>
+
+class Foo
+{
+public:
+    void foo(double, bool = true)
+    {
+        spdlog::info("double, bool = true");
+    }
+
+    void foo(double, std::string const&)
+    {
+        spdlog::info("double, bool = true");
+    }
+};
+
+int main()
+{
+    Foo f;
+
+    f.foo(0.0f, "lala");
+
+    return EXIT_SUCCESS;
+}
