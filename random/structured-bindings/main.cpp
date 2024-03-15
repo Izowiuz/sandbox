5538d3e7b8ce0b725ac229b7b7de34a10c29a4e8
Jakub Narolewski <izowiuz@gmail.com>
2024-03-15 23:58:22 +0100

    stuff!

---
+++ random/structured-bindings/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <unordered_map>
+
+#include "spdlog/spdlog.h"
+
+template <typename Key, typename Value>
+void insert(std::unordered_map<Key, Value>& map, Key const& key, Value const& value)
+{
+    if (auto const [it, flag] = map.insert({ key, value }); flag == true) {
+        spdlog::info("inserted! [{}, {}]", key, value);
+    } else {
+        spdlog::info("not inserted! [{}, {}]", key, value);
+    }
+}
+
+int main()
+{
+    std::unordered_map<std::int32_t, std::int32_t> map;
+
+    insert(map, 1, 1);
+    insert(map, 2, 2);
+    insert(map, 2, 3);
+
+    return EXIT_SUCCESS;
+}
