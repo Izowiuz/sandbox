e4beae05ad61f73ffab457b7159d3dbf042dadd6
Jakub Narolewski <izowiuz@gmail.com>
2025-03-25 10:52:13 +0100

    siup v14

--- create_sandbox_target.cmake
+++ create_sandbox_target.cmake
@@ -1,5 +1,10 @@
 function(create_sandbox_target tag target_name)
 
+set(options "")
+set(one_value_args "ADDITIONAL_COMPILE_FLAGS")
+set(multi_value_args "")
+cmake_parse_arguments(PARSE_ARGV 0 arg "${options}" "${one_value_args}" "${multi_value_args}")
+
 set(SANDBOX_TARGET_PATH "${CMAKE_CURRENT_SOURCE_DIR}/${tag}/${target_name}/")
 message("Target path: ${SANDBOX_TARGET_PATH}")
 
@@ -8,7 +13,6 @@ set(full_target_name ${tag}-${target_name})
 if (NOT EXISTS "${SANDBOX_TARGET_PATH}")
     message(FATAL_ERROR "Directory: ${SANDBOX_TARGET_PATH} do not exists.")
 endif()
-
 
 file(
 GLOB_RECURSE
@@ -95,6 +99,12 @@ if (EXISTS "${SANDBOX_TARGET_PATH}/main.cpp")
     PROPERTY
         CXX_STANDARD 20
     )
+
+    if (arg_ADDITIONAL_COMPILE_FLAGS)
+        message("For target: ${target_name} - ADDITIONAL_COMPILE_FLAGS set to: ${arg_ADDITIONAL_COMPILE_FLAGS}")
+        target_compile_options(${full_target_name} PRIVATE ${arg_ADDITIONAL_COMPILE_FLAGS})
+    endif()
+
 else()
     message("No main found for: ${full_target_name}")
 endif()
---
+++ random/modules-test/main.cpp
@@ -0,0 +1,7 @@
+// does not work :[
+// import std;
+
+// int main()
+// {
+//     return 0;
+// }
