7e500c658088c2bb426e1534493b404291f7162f
Jakub Narolewski <izowiuz@gmail.com>
2025-10-05 23:20:43 +0200

    siup v24

---
+++ linkedin-learning-functional-programming-in-c++/challenge-build-a-functional-pipeline/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <ranges>
+#include <vector>
+
+#include "fmt/ranges.h"
+#include "spdlog/spdlog.h"
+
+int main()
+{
+    // auto const vec = std::ranges::views::iota(1, 101) | std::ranges::to<std::vector>();
+    auto pipeline = std::ranges::views::iota(1, 101) |
+                    std::ranges::views::transform([](int element) { return element * element; }) |
+                    std::ranges::views::filter([](int element) { return element % 5 == 0; }) |
+                    std::ranges::views::take(10);
+
+    for (const auto& element : pipeline) {
+        spdlog::info("element: {}", element);
+    }
+
+    return EXIT_SUCCESS;
+}
