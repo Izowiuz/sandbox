579ab8307de6c253c07e64d2da460391ae9f58cd
Jakub Narolewski <izowiuz@gmail.com>
2025-10-08 21:54:08 +0200

    siup v29

--- create_sandbox_target.cmake
+++ create_sandbox_target.cmake
@@ -2,7 +2,7 @@ function(create_sandbox_target tag target_name)
 
 set(options "")
 set(one_value_args "ADDITIONAL_COMPILE_FLAGS")
-set(multi_value_args "")
+set(multi_value_args "ADDITIONAL_LIBRARIES")
 cmake_parse_arguments(PARSE_ARGV 0 arg "${options}" "${one_value_args}" "${multi_value_args}")
 
 set(sandbox_target_path "${CMAKE_CURRENT_SOURCE_DIR}/${tag}/${target_name}/")
@@ -118,6 +118,11 @@ if (EXISTS "${sandbox_target_path}/main.cpp")
     if (arg_ADDITIONAL_COMPILE_FLAGS)
         message("For target: ${target_name} - ADDITIONAL_COMPILE_FLAGS set to: ${arg_ADDITIONAL_COMPILE_FLAGS}")
         target_compile_options(${full_target_name} PRIVATE ${arg_ADDITIONAL_COMPILE_FLAGS})
+    endif()
+
+    if (arg_ADDITIONAL_LIBRARIES)
+        message("For target: ${target_name} - arg_ADDITIONAL_LIBRARIES set to: ${arg_ADDITIONAL_LIBRARIES}")
+        target_link_libraries(${full_target_name} PRIVATE ${arg_ADDITIONAL_LIBRARIES})
     endif()
 
 else()
