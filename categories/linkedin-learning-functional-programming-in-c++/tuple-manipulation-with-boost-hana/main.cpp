a9b24e66d3a392b239212fe6b14b8a8450423054
Jakub Narolewski <izowiuz@gmail.com>
2025-10-08 23:19:01 +0200

    siup v32

--- categories/linkedin-learning-functional-programming-in-c++/tuple-manipulation-with-boost-hana/main.cpp
+++ categories/linkedin-learning-functional-programming-in-c++/tuple-manipulation-with-boost-hana/main.cpp
@@ -1,8 +1,22 @@
 #include <cstdlib>
+#include <string>
 
+#include "boost/hana.hpp"
 #include "spdlog/spdlog.h"
 
 int main()
 {
+    auto my_tuple = boost::hana::make_tuple(10, "hello", 3.14);
+    auto transformed_tuple = boost::hana::transform(my_tuple, [](auto x) {
+        if constexpr (std::is_same_v<decltype(x), const char*>) {
+            std::string str(x);
+            return str + " - " + str;
+        } else {
+            return x + x;
+        }
+    });
+
+    boost::hana::for_each(transformed_tuple, [](auto x) { spdlog::info("x: {}", x); });
+
     return EXIT_SUCCESS;
 }
