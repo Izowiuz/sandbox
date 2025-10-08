d4ac30d8c9e756cce5bb912e97b7d0190d1c2c4b
Jakub Narolewski <izowiuz@gmail.com>
2025-10-08 21:28:08 +0200

    siup v28

---
+++ linkedin-learning-functional-programming-in-c++/challenge-function-composition-pipeline/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+
+#include "spdlog/spdlog.h"
+
+auto compose(auto f1, auto f2, auto f3)
+{
+    return [f1, f2, f3](int x) { return f1(f2(f3(x))); };
+}
+
+int main()
+{
+    auto decrement = [](int x) { return x - 1; };
+    auto double_value = [](int x) { return x * 2; };
+    auto add_3 = [](int x) { return x + 3; };
+    auto pipeline = compose(decrement, double_value, add_3);
+
+    spdlog::info("pipeline result for 5: {}", pipeline(5));
+
+    return EXIT_SUCCESS;
+}
