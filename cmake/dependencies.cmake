61e354297bd669fce28a67a016e5cb452e062391
Jakub Narolewski <izowiuz@gmail.com>
2025-10-11 00:22:49 +0200

    siup v37

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,7 +8,8 @@ FetchContent_Declare(
     RxCpp
     GIT_REPOSITORY "https://github.com/ReactiveX/RxCpp.git"
     GIT_TAG "v4.1.1"
-    PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/patches/rx-notification.hpp.const_rxu_error_ptr.diff
+    PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/patches/rxcpp-rx-notification.hpp.const_rxu_error_ptr.diff &&
+    PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/patches/rxcpp-disable-tests-and-examples.diff
     UPDATE_DISCONNECTED 1
 )
 FetchContent_MakeAvailable(RxCpp)
