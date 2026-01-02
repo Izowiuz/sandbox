aff0f2a39741296b6c66c0c27072aa20ec7f594b
Jakub Narolewski <izowiuz@gmail.com>
2026-01-02 18:58:24 +0100

    siup v58

--- cmake/create_sandbox_target.cmake
+++ cmake/create_sandbox_target.cmake
@@ -52,132 +52,138 @@ if(NOT EXISTS "${main_cpp}")
     )
 endif()
 
-file(
-GLOB_RECURSE
-    source_files
-    "${sandbox_target_path}/src/*.*"
-)
+set("category-${tag}-enabled" ON CACHE BOOL "Enable or disable ${tag} category.")
 
-file(
-GLOB_RECURSE
-    tests_sources
-    "${sandbox_target_path}/*Tests.cpp"
-)
-
-LIST(LENGTH tests_sources tests_sources_count)
-if (tests_sources_count)
-    message(STATUS "Found tests for: ${full_target_name}")
-
-    add_executable(
-        ${full_target_name}-tests
+if(DEFINED "category-${tag}-enabled")
+    file(
+            GLOB_RECURSE
+            source_files
+            "${sandbox_target_path}/src/*.*"
     )
 
-    target_sources(
-        ${full_target_name}-tests
-    PRIVATE
-        ${source_files}
-        ${tests_sources}
+    file(
+            GLOB_RECURSE
+            tests_sources
+            "${sandbox_target_path}/*Tests.cpp"
     )
 
-    target_include_directories(
-        ${full_target_name}-tests
-    PRIVATE
-        $<BUILD_INTERFACE:${sandbox_target_path}/src>
-    )
+    LIST(LENGTH tests_sources tests_sources_count)
+    if (tests_sources_count)
+        message(STATUS "Found tests for: ${full_target_name}")
 
-    target_link_libraries(
-        ${full_target_name}-tests
-    PRIVATE
-        spdlog::spdlog
-        GTest::gtest
-        GTest::gtest_main
-    )
-
-    set_target_properties(
-        ${full_target_name}-tests
-    PROPERTIES
-        FOLDER "${tag}"
-    )
-
-else()
-    message(STATUS "No tests found for: ${full_target_name}")
-endif()
-
-if (EXISTS "${sandbox_target_path}/main.cpp")
-    message(STATUS "Found main.cpp for: ${full_target_name}")
-
-    add_executable(
-        ${full_target_name}
-    )
-
-    # cxx standard override
-    if (arg_CXX_STANDARD)
-        set_target_properties(${full_target_name} PROPERTIES
-            CXX_STANDARD ${arg_CXX_STANDARD}
-            CXX_STANDARD_REQUIRED YES
+        add_executable(
+                ${full_target_name}-tests
         )
+
+        target_sources(
+                ${full_target_name}-tests
+                PRIVATE
+                ${source_files}
+                ${tests_sources}
+        )
+
+        target_include_directories(
+                ${full_target_name}-tests
+                PRIVATE
+                $<BUILD_INTERFACE:${sandbox_target_path}/src>
+        )
+
+        target_link_libraries(
+                ${full_target_name}-tests
+                PRIVATE
+                spdlog::spdlog
+                GTest::gtest
+                GTest::gtest_main
+        )
+
+        set_target_properties(
+                ${full_target_name}-tests
+                PROPERTIES
+                FOLDER "${tag}"
+        )
+
     else()
-        set_target_properties(${full_target_name} PROPERTIES
-            CXX_STANDARD ${CMAKE_CXX_STANDARD}
-            CXX_STANDARD_REQUIRED YES
-        )
+        message(STATUS "No tests found for: ${full_target_name}")
     endif()
 
-    target_sources(
-        ${full_target_name}
-    PRIVATE
-        ${source_files}
-        ${sandbox_target_path}/main.cpp
-    )
+    if (EXISTS "${sandbox_target_path}/main.cpp")
+        message(STATUS "Found main.cpp for: ${full_target_name}")
 
-    target_include_directories(
-        ${full_target_name}
-    PRIVATE
-        $<BUILD_INTERFACE:${sandbox_target_path}/src>
-    )
-
-    target_link_libraries(
-        ${full_target_name}
-    PRIVATE
-        spdlog::spdlog
-    )
-
-    set_target_properties(
-        ${full_target_name}
-    PROPERTIES
-        FOLDER "${tag}"
-    )
-
-    if (arg_ADDITIONAL_COMPILE_FLAGS)
-        message(STATUS "For target: ${full_target_name} - ADDITIONAL_COMPILE_FLAGS set to: ${arg_ADDITIONAL_COMPILE_FLAGS}")
-        target_compile_options(${full_target_name} PRIVATE ${arg_ADDITIONAL_COMPILE_FLAGS})
-    endif()
-
-    if (arg_ADDITIONAL_LIBRARIES)
-        message(STATUS "For target: ${full_target_name} - ADDITIONAL_LIBRARIES set to: ${arg_ADDITIONAL_LIBRARIES}")
-        target_link_libraries(${full_target_name} PRIVATE ${arg_ADDITIONAL_LIBRARIES})
-    endif()
-
-    # conditionally enable ASAN address and undefined sanitizers
-    if(DEFINED ENV{SANDBOX_ENABLE_ASAN} AND "$ENV{SANDBOX_ENABLE_ASAN}" STREQUAL "ON")
-        message(STATUS "ASAN and UBSAN enabled for target: ${full_target_name}")
-        add_compile_options(-fsanitize=address,undefined -fno-omit-frame-pointer)
-
-        target_compile_options(
-            ${full_target_name}
-        PRIVATE
-            -fsanitize=address,undefined -fno-omit-frame-pointer
+        add_executable(
+                ${full_target_name}
         )
 
-        target_link_options(
-            ${full_target_name}
-        PRIVATE
-            -fsanitize=address,undefined
-        )
-    endif()
+        # cxx standard override
+        if (arg_CXX_STANDARD)
+            set_target_properties(${full_target_name} PROPERTIES
+                    CXX_STANDARD ${arg_CXX_STANDARD}
+                    CXX_STANDARD_REQUIRED YES
+            )
+        else()
+            set_target_properties(${full_target_name} PROPERTIES
+                    CXX_STANDARD ${CMAKE_CXX_STANDARD}
+                    CXX_STANDARD_REQUIRED YES
+            )
+        endif()
 
+        target_sources(
+                ${full_target_name}
+                PRIVATE
+                ${source_files}
+                ${sandbox_target_path}/main.cpp
+        )
+
+        target_include_directories(
+                ${full_target_name}
+                PRIVATE
+                $<BUILD_INTERFACE:${sandbox_target_path}/src>
+        )
+
+        target_link_libraries(
+                ${full_target_name}
+                PRIVATE
+                spdlog::spdlog
+        )
+
+        set_target_properties(
+                ${full_target_name}
+                PROPERTIES
+                FOLDER "${tag}"
+        )
+
+        if (arg_ADDITIONAL_COMPILE_FLAGS)
+            message(STATUS "For target: ${full_target_name} - ADDITIONAL_COMPILE_FLAGS set to: ${arg_ADDITIONAL_COMPILE_FLAGS}")
+            target_compile_options(${full_target_name} PRIVATE ${arg_ADDITIONAL_COMPILE_FLAGS})
+        endif()
+
+        if (arg_ADDITIONAL_LIBRARIES)
+            message(STATUS "For target: ${full_target_name} - ADDITIONAL_LIBRARIES set to: ${arg_ADDITIONAL_LIBRARIES}")
+            target_link_libraries(${full_target_name} PRIVATE ${arg_ADDITIONAL_LIBRARIES})
+        endif()
+
+        # conditionally enable ASAN address and undefined sanitizers
+        if(DEFINED ENV{SANDBOX_ENABLE_ASAN} AND "$ENV{SANDBOX_ENABLE_ASAN}" STREQUAL "ON")
+            message(STATUS "ASAN and UBSAN enabled for target: ${full_target_name}")
+            add_compile_options(-fsanitize=address,undefined -fno-omit-frame-pointer)
+
+            target_compile_options(
+                    ${full_target_name}
+                    PRIVATE
+                    -fsanitize=address,undefined -fno-omit-frame-pointer
+            )
+
+            target_link_options(
+                    ${full_target_name}
+                    PRIVATE
+                    -fsanitize=address,undefined
+            )
+        endif()
+
+    else()
+        message(STATUS "No main found for: ${full_target_name}")
+    endif()
 else()
-    message(STATUS "No main found for: ${full_target_name}")
+    message(STATUS "Category: ${tag} is disabled.")
 endif()
 
 endfunction()
